5e3b3516b32d303594575f25cdef6a5081d4c690
Alex <kavanokami@gmail.com>
2020-07-01 19:18:23 +0400

    feat: add password input field

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,15 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField/TextField';
+import {
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from '@material-ui/core';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const useStyles = makeStyles((theme) => ({
   login: {
@@ -25,11 +34,40 @@ const useStyles = makeStyles((theme) => ({
   },
   form__input: {
     width: '100%',
+    margin: '0 0 28px',
   },
 }));
 
+interface State {
+  email: String;
+  password: String;
+  showPassword: Boolean;
+}
+
 const Login = (): JSX.Element => {
   const classes = useStyles();
+  const [values, setValues] = React.useState<State>({
+    email: '',
+    password: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop: keyof State) => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
     <div className={classes.login}>
       <div className={classes.form}>
@@ -39,11 +77,37 @@ const Login = (): JSX.Element => {
         <form>
           <TextField
             label='Email'
-            variant='outlined'
             type='email'
+            value={values.email}
+            onChange={handleChange('email')}
+            variant='outlined'
             required
             className={classes.form__input}
           />
+          <FormControl className={classes.form__input} variant='outlined'>
+            <InputLabel htmlFor='outlined-adornment-password'>
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id='outlined-adornment-password'
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='toggle password visibility'
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge='end'
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={70}
+            />
+          </FormControl>
         </form>
       </div>
     </div>
