7fe69a9f3cec2b880698a4bec8d6968f81b9aef6
Alex <kavanokami@gmail.com>
2020-07-01 20:02:27 +0400

    feat: add submit button to Login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField/TextField';
 import {
   FormControl,
   InputLabel,
   OutlinedInput,
   InputAdornment,
   IconButton,
+  Button,
+  Typography,
+  TextField,
 } from '@material-ui/core';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 const useStyles = makeStyles((theme) => ({
-  login: {
+  loginBackground: {
     display: 'flex',
     width: '100vw',
     height: '100vh',
     backgroundColor: theme.palette.greyBackground.main,
   },
-  form: {
+  loginContents: {
     width: 396,
     height: 376,
     margin: 'auto',
@@ -28,13 +29,21 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#fff',
     boxShadow: '-1px 1px 5px 0px rgba(0,0,0,0.75)',
   },
-  form__title: {
+  title: {
     margin: '0 0 48px',
     color: '#3D5170',
+  },
+  form: {
+    textAlign: 'right',
   },
   form__input: {
     width: '100%',
     margin: '0 0 28px',
+  },
+  form__submit: {
+    width: 99,
+    height: 36,
+    padding: '10px 15px',
   },
 }));
 
@@ -69,12 +78,12 @@ const Login = (): JSX.Element => {
   };
 
   return (
-    <div className={classes.login}>
-      <div className={classes.form}>
-        <Typography variant='h5' component='h1' className={classes.form__title}>
+    <div className={classes.loginBackground}>
+      <div className={classes.loginContents}>
+        <Typography variant='h5' component='h1' className={classes.title}>
           Авторизация
         </Typography>
-        <form>
+        <form className={classes.form}>
           <TextField
             label='Email'
             type='email'
@@ -105,9 +114,17 @@ const Login = (): JSX.Element => {
                   </IconButton>
                 </InputAdornment>
               }
+              required
               labelWidth={70}
             />
           </FormControl>
+          <Button
+            variant='contained'
+            color='primary'
+            className={classes.form__submit}
+          >
+            Войти
+          </Button>
         </form>
       </div>
     </div>
