ceefef7ba04c7f311f889984f71a24fc58ce8647
Alex <kavanokami@gmail.com>
2020-07-02 15:12:08 +0400

    feat: add breakpoints for grid

--- src/components/companies/Companies.tsx
+++ src/components/companies/Companies.tsx
@@ -4,16 +4,22 @@ import Header from './Header';
 import Table from './Table';
 
 import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  grid: {
-    display: 'grid',
-    gridTemplateColumns: '100px auto',
-    gridTemplateRows: '100px auto',
-    minHeight: '100vh',
-  },
-});
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    grid: {
+      display: 'grid',
+      gridTemplateColumns: '100px auto',
+      gridTemplateRows: '100px auto',
+      minHeight: '100vh',
+      [theme.breakpoints.down(600)]: {
+        gridTemplateColumns: 'auto',
+        gridTemplateRows: '100px 100px auto',
+      },
+    },
+  })
+);
 
 const Companies = () => {
   const classes = useStyles();
--- src/components/companies/Sidebar.tsx
+++ src/components/companies/Sidebar.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, createStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  sidebar: {
-    gridRow: '1 / 3',
-  },
-});
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    sidebar: {
+      gridRow: '1 / 3',
+      background: 'red',
+      [theme.breakpoints.down(600)]: {
+        gridRow: '1 / 2',
+        background: 'yellow',
+      },
+    },
+  })
+);
 
 const Sidebar = () => {
   const classes = useStyles();
-  return (
-    <div className={classes.sidebar} style={{ background: 'red' }}>
-      Sidebar
-    </div>
-  );
+  return <div className={classes.sidebar}>Sidebar</div>;
 };
 
 export default Sidebar;
