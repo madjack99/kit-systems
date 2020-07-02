662ecf0d8282abfd62535ac94bb15e23ec1ff5ea
Alex <kavanokami@gmail.com>
2020-07-02 13:02:28 +0400

    init: Companies component

---
+++ src/components/companies/Companies.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
+import Table from './Table';
+
+const Companies = () => {
+  return <div>Companies!!</div>;
+};
+
+export default Companies;
--- src/pages/CompaniesPage.tsx
+++ src/pages/CompaniesPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Companies from '../components/companies/Companies';
 
 const CompaniesPage = () => {
-  return <div>Companies</div>;
+  return <Companies />;
 };
 
 export default CompaniesPage;
