15a1d266488a9bc6d52a1f570cee75bd7e94769c
Alex <kavanokami@gmail.com>
2020-07-02 18:48:59 +0400

    feat: add styles for table

--- src/components/companies/Table.tsx
+++ src/components/companies/Table.tsx
@@ -1,39 +1,52 @@
 import React from 'react';
 import MaterialTable from 'material-table';
 
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  tableWrapper: {
+    padding: '47px 78px 99px 69px',
+    backgroundColor: theme.palette.greyBackground.main,
+  },
+}));
+
 const Table = () => {
+  const classes = useStyles();
+
   return (
-    <MaterialTable
-      title='Title'
-      options={{
-        actionsColumnIndex: 5,
-        search: false,
-        showTitle: false,
-      }}
-      columns={[
-        { title: 'Наименование компании', field: 'name' },
-        { title: 'Тип юр.лица', field: 'type' },
-        { title: 'Регион', field: 'region' },
-        { title: 'Город', field: 'city' },
-      ]}
-      data={[
-        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      ]}
-      actions={[
-        {
-          icon: 'delete',
-          tooltip: 'Delete user',
-          onClick: (event, rowData) => {
-            console.log('click');
+    <div className={classes.tableWrapper}>
+      <MaterialTable
+        title='Title'
+        options={{
+          actionsColumnIndex: 5,
+          search: false,
+          showTitle: false,
+        }}
+        columns={[
+          { title: 'Наименование компании', field: 'name' },
+          { title: 'Тип юр.лица', field: 'type' },
+          { title: 'Регион', field: 'region' },
+          { title: 'Город', field: 'city' },
+        ]}
+        data={[
+          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        ]}
+        actions={[
+          {
+            icon: 'delete',
+            tooltip: 'Delete user',
+            onClick: (event, rowData) => {
+              console.log('click');
+            },
           },
-        },
-        {
-          icon: 'edit',
-          tooltip: 'Edit user',
-          onClick: (event, rowData) => {},
-        },
-      ]}
-    />
+          {
+            icon: 'edit',
+            tooltip: 'Edit user',
+            onClick: (event, rowData) => {},
+          },
+        ]}
+      />
+    </div>
   );
 };
 
