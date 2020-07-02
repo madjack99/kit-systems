8ec9737da2739e0f3113abf60baa7f2eb1c4f61b
Alex <kavanokami@gmail.com>
2020-07-02 11:37:03 +0400

    feat: add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import LoginPage from './pages/LoginPage';
 import CompaniesPage from './pages/CompaniesPage';
 
 function App() {
   return (
-    <div className='App'>
-      <LoginPage />
-      <CompaniesPage />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <LoginPage />
+        </Route>
+        <Route path='/companies'>
+          <CompaniesPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
